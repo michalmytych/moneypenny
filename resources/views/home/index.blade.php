0340fbb97b5a0749c5ab3519bb8d2cc96fc1aff7
mike <michal.mytych@codeclutch.dev>
2023-05-05 22:56:05 +0200

    Update Home page #2

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,3 +1,11 @@
+@push('styles')
+    <style>
+        body {
+            overflow: hidden;
+        }
+    </style>
+@endpush
+
 <x-app-layout>
     <div class="py-8 w-3/4 mx-auto">
         <div class="w-full mx-auto sm:px-6 lg:px-8">

dce65d9db7425090fe184877f5c1bb8d3f545fbc
mike <michal.mytych@codeclutch.dev>
2023-05-09 03:47:46 +0200

    Add periodic synchronization & related supervisor config

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
+use App\Jobs\Synchronization\PeriodicSynchronization;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
@@ -12,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->job(new PeriodicSynchronization())->everyFiveMinutes();
     }
 
     /**

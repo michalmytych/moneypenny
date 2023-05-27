a194a271d2fc48c45505574715f349568d8f6e85
mike <michal.mytych@codeclutch.dev>
2023-05-27 14:36:43 +0200

    Update limba deploy script #2

--- scripts/deploy/limba.sh
+++ scripts/deploy/limba.sh
@@ -2,7 +2,10 @@
 
 cd ~/moneypenny && \
 git pull origin bachelor && \
+composer install && \
+composer du && \
 php artisan migrate && \
 php artisan optimize:clear && \
+npm install && \
 npm run build && \
 php artisan moneypenny:create-users-personal-accounts

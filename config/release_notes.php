a1b21dd962d1338295a4747019994c00d881eb68
mike <michal.mytych@codeclutch.dev>
2023-06-15 20:38:45 +0200

    Changed release notes

--- config/release_notes.php
+++ config/release_notes.php
@@ -34,16 +34,15 @@ return [
             ]
         ],
         [
-            'header' => 'Moneypenny v0.2',
+            'header' => '🐥🐥🐥 Moneypenny v0.2',
             'date' => '10-05-2023 22:15',
             'notes' => [
                 'Code refactor',
                 'Added users devices tracking',
-                'Added users devices tracking'
             ]
         ],
         [
-            'header' => 'Moneypenny v0.1',
+            'header' => '🐤🐤🐤 Moneypenny v0.1',
             'date' => '06-05-2023 00:15',
             'notes' => [
                 'Init application'

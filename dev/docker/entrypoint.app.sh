128d79e6ce8e781ce5b1cbed0c6a56e24a27b3bb
mike <michamytych@gmail.com>
2023-12-28 00:45:30 +0100

    Add dev & docker configs

---
+++ dev/docker/entrypoint.app.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+env
+
+if [[ -n "$1" ]]; then
+    exec "$@"
+else
+    composer install
+    wait-for-it db:3306 -t 45
+    php artisan migrate --database=mysql
+    chown -R www-data:www-data storage
+    exec apache2-foreground
+fi
